1842bab0cfe72c5bd9f3feed33d938275b80597a
Joseph Dvorak <dvorakjt@gmail.com>
2020-07-11 22:49:20 -0400

    table now displays different icon depending on affiliation

--- src/Components/TableRow/TableRow.js
+++ src/Components/TableRow/TableRow.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './style.css';
+import goodEmote from "../../images/goodEmote.png";
+import evilEmote from "../../images/evilEmote.png";
+import neutEmote from "../../images/neutEmote.png";
 
 function TableRow(props) {
+    console.log("../src/images/" + props.affiliation.toLowerCase().slice(0, 4) + "Emote.png");
     return (
         <>
             <tr className="topOfBox">
@@ -18,7 +22,16 @@ function TableRow(props) {
                     {props.anime}
                 </td>
                 <td>
-                    {props.affiliation}
+                    <img src={(() => {
+                        if (props.affiliation.toLowerCase().includes("good")) {
+                            return goodEmote;
+                        } else if (props.affiliation.toLowerCase().includes("evil")) {
+                            return evilEmote;
+                        } else {
+                            return neutEmote;
+                        }
+                    })()}
+                        alt={props.affiliation} />
                 </td>
             </tr>
             <tr>
